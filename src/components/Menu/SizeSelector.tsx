1d058c01e4d6d5e01086e23833078247bfbaecf5
SurushTodzhibekov <soorush86@gmail.com>
2025-12-03 17:58:20 +0500

    add SizeSelector component for selecting pizza sizes

---
+++ src/components/Menu/SizeSelector.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  COLORS,
+  TYPOGRAPHY,
+  PIZZA_SIZES,
+  PizzaSize,
+} from "../../constants/designTokens";
+
+interface SizeSelectorProps {
+  selectedSize: PizzaSize;
+  onSelect: (size: PizzaSize) => void;
+}
+
+/**
+ * SizeSelector Component
+ * Allows users to select pizza size (22, 28, 33 cm)
+ */
+export const SizeSelector: React.FC<SizeSelectorProps> = ({
+  selectedSize,
+  onSelect,
+}) => {
+  return (
+    <div className="flex justify-center gap-[37px] mb-6">
+      {PIZZA_SIZES.map((size) => (
+        <button
+          key={size}
+          onClick={() => onSelect(size)}
+          className="w-10 h-10 rounded-full flex items-center justify-center transition-all"
+          style={{
+            fontSize: TYPOGRAPHY.fontSize.sizeButton,
+            background:
+              selectedSize === size
+                ? COLORS.gradientPrimary
+                : COLORS.transparent,
+            border:
+              selectedSize === size ? "none" : `1.5px solid ${COLORS.textGray}`,
+            color: selectedSize === size ? COLORS.white : COLORS.textGray,
+          }}
+        >
+          {size}
+        </button>
+      ))}
+    </div>
+  );
+};
